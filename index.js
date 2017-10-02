33c11fefca372e924bef88d6dd63472e590cefbb
Julianne Kim <juliannne@gmail.com>
2017-10-02 15:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var lis = document.querySelectorAll(`div #grand-node`)
+  var list = document.getElementById('grand-node')
+  // var lis = document.querySelectorAll(`#grand-node div`)
   if (!lis.length) {
     return false;
   } else {
